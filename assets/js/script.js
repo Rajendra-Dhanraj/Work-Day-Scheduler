83b7af7a884d92b5d1fd081bba79ef0071621df3
Rajendra Dhanraj <rajiedhan@Rajendras-MacBook-Pro.local>
2021-01-10 22:57:04 -0500

    compare current time with block time and change color

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,21 +3,44 @@ $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 // listener for save button click
 $(".saveBtn").on("click", function () {
+  // variables to get value and store in localStorage
+  var task = $(this).siblings(".description").val();
+  var time = $(this).parent().attr("id");
 
-// variables to get value and store in localStorage
-var task = $(this).siblings(".description").val();
-var time = $(this).parent().attr("id");
-
-localStorage.setItem(time, task);
+  localStorage.setItem(time, task);
 });
 
 // load local storage into webpage
-$('#9oclock .description').val(localStorage.getItem('9oclock'));
-$('#10oclock .description').val(localStorage.getItem('10oclock'));
-$('#11oclock .description').val(localStorage.getItem('11oclock'));
-$('#12oclock .description').val(localStorage.getItem('12oclock'));
-$('#1oclock .description').val(localStorage.getItem('1oclock'));
-$('#2oclock .description').val(localStorage.getItem('2oclock'));
-$('#3oclock .description').val(localStorage.getItem('3oclock'));
-$('#4oclock .description').val(localStorage.getItem('4oclock'));
-$('#5oclock .description').val(localStorage.getItem('5oclock'));
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
+
+function colorChange() {
+  // get current hour as a number
+  var currentHour = moment().hours();
+  // loop over time blocks
+  $(".time-block").each(function () {
+    //convert block id into number
+    var blockHour = parseInt($(this).attr("id"));
+    console.log(blockHour, currentHour);
+    // evaluate current time vs: past, present, future and change classes accordingly
+    if (blockHour < currentHour) {
+      $(this).addClass("past");
+    } else if (blockHour === currentHour) {
+      $(this).removeClass("past");
+      $(this).addClass("present");
+    } else {
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+      $(this).addClass("future");
+    }
+  });
+}
+
+colorChange();
